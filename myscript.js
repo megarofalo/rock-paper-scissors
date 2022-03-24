0eb06512933084fd3a04e6c8ecfb737608c3d492
megarofalo <megarofal03@gmail.com>
2022-03-24 15:17:33 -0400

    Create full game of rock paper scissors

--- myscript.js
+++ myscript.js
@@ -1,6 +1,15 @@
 
 // Randomly either return 'rock', 'paper, or 'scissors'
 
+
+let computerSelection;
+let playerSelection;
+
+function questionAsk() {
+    const question = prompt("Choose your player").toLowerCase();
+    return question;
+}
+
 function computerPlay() {
     let randomNum = Math.floor(Math.random() * 100);
     let choosePlayer = '';
@@ -20,32 +29,79 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    playerSelection = questionAsk();
     let output = '';
+    let score = '';
+    let noScore = 0;
+    let homeScore = 1;
+    let oppScore = 2;
+
+
     if (playerSelection === computerSelection) {
-        output = "You both chose " + playerSelection + " please try again.";
+        output = "You both chose " + playerSelection + " please try again."; 
+        score = noScore;
     }
     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         output = "You won! Rock beats scissors";
+        score = homeScore;
     }
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
         output = "You won! Paper beats rock";
+        score = homeScore;
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         output = "You won! Scissors beats paper";
+        score = homeScore;
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         output = "You lost! Rock beats scissors";
+        score = oppScore;
     }
-    else if(playerSelection === 'rock' && computerSelection === 'paper') {
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
         output = "You lost! Paper beats rock";
+        score = oppScore;
     }
     else {
         output = "You lost! Scissors beats paper";
+        score = oppScore;
     }
-
-    return output;
+    console.log(output);
+    return score;
 }
 
-const computerSelection = computerPlay();
-const playerSelection = prompt("Choose your player").toLowerCase();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+let urScore = 0;
+let pcScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let output = (playRound(playerSelection, computerSelection));
+        if (output === 0) {
+            urScore;
+            pcScore;
+        }
+        else if (output === 1) {
+            urScore++;
+        }
+        else if (output === 2) {
+            pcScore++;
+        }
+
+        console.log("Your score: " + urScore + " Computer score: " + pcScore);
+    }
+
+    if (urScore === pcScore) {
+        console.log("It's a tie!");
+    }
+    else if (urScore < pcScore) {
+        console.log("Sorry you lost.");
+    }
+    else {
+        console.log("Congrats you won!");
+    }
+
+}
+
+
+
+console.log(game());
