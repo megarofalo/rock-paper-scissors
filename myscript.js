731c3efe37e26e644b4534c3042fd3ffd2720b64
megarofalo <megarofal03@gmail.com>
2022-03-24 12:33:15 -0400

    Create playerSelection function

--- myscript.js
+++ myscript.js
@@ -1,9 +1,26 @@
 
-function computerPlay() {
+// Randomly either return 'rock', 'paper, or 'scissors'
 
+function computerPlay() {
+    let randomNum = Math.floor(Math.random() * 100);
+    let choosePlayer = '';
+
+    if (randomNum >= 0 && randomNum <= 33) {
+        choosePlayer = 'rock';
+    }
+    else if (randomNum >= 34 && randomNum <= 67) {
+        choosePlayer = 'scissors';
+    }
+    
+    else {
+        choosePlayer = 'paper';
+    }
+ return choosePlayer;
 }
 
-function singleRound() {
+console.log(computerPlay());
 
+function playRound() {
+    
 }
 
