5ee7ae33b6de1b0eda8f0d9611abb9e812fd1c52
megarofalo <megarofal03@gmail.com>
2022-03-24 13:14:33 -0400

    Create playRound function and output game result

--- myscript.js
+++ myscript.js
@@ -11,16 +11,41 @@ function computerPlay() {
     else if (randomNum >= 34 && randomNum <= 67) {
         choosePlayer = 'scissors';
     }
-    
+
     else {
         choosePlayer = 'paper';
     }
- return choosePlayer;
+    return choosePlayer;
 }
 
-console.log(computerPlay());
 
-function playRound() {
-    
+function playRound(playerSelection, computerSelection) {
+    let output = '';
+    if (playerSelection === computerSelection) {
+        output = "You both chose " + playerSelection + " please try again.";
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        output = "You won! Rock beats scissors";
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        output = "You won! Paper beats rock";
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        output = "You won! Scissors beats paper";
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        output = "You lost! Rock beats scissors";
+    }
+    else if(playerSelection === 'rock' && computerSelection === 'paper') {
+        output = "You lost! Paper beats rock";
+    }
+    else {
+        output = "You lost! Scissors beats paper";
+    }
+
+    return output;
 }
 
+const computerSelection = computerPlay();
+const playerSelection = prompt("Choose your player").toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
